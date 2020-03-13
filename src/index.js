5ca491f850e866ac50c56c5cf219d43b2d2e8091
Alex <alexg1990@users.noreply.github.com>
2020-03-13 17:06:14 +0100

    added modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+app.get('/users', (req, res) => res.send("Hello World"));
+app.post('/users', (req, res) => res.send("Post"));
+app.put('/users/:userId', (req, res) => res.send(`put ${req.params.userId}`));
+app.delete('/users/:usedId', (req, res) => res.send(`delete ${req.params.userId}`));
+
+app.listen(process.env.PORT, () => console.log("Listening on port 3000"));
 
 console.log('Hello Node.js project.');
-console.log(process.env.MY_SECRET);
